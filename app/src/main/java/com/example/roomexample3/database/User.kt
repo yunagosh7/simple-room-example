810d9334a817f5622250b4769027b33ce4f66b83
Vila Juan Cruz <yunagosh7@gmail.com>
2023-04-02 12:37:23 -0300

    Initial commit

---
+++ app/src/main/java/com/example/roomexample3/database/User.kt
@@ -0,0 +1,14 @@
+package com.example.roomexample3.database
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class User (
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "uid")
+    val uid: Int,
+    @ColumnInfo(name = "first_name") val name: String?,
+    @ColumnInfo(name = "last_name") val lastName: String?
+        )
---
+++ app/src/main/java/com/example/roomexample3/database/UserDao.kt
@@ -0,0 +1,21 @@
+package com.example.roomexample3.database
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
+interface UserDao {
+    @Query("SELECT * FROM user")
+    fun getAll(): List<User>
+
+    @Query("SELECT * FROM user WHERE first_name LIKE :first AND last_name LIKE :last")
+    fun getByName(first: String, last: String): User
+
+    @Insert
+    fun insertAll(vararg users: User)
+
+    @Delete
+    fun delete(user: User)
+}
